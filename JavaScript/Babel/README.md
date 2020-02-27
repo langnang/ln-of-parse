ff32bd34aed1dd183c368dfa2787d4e5c9a66cde
Langlang.Chen <langlang.chen@advantech.com.cn>
2020-02-27 15:55:13 +0800

    init

---
+++ JavaScript/Babel/README.md
@@ -0,0 +1,3 @@
+<h1>ES6 <small>test</small></h1>
+
+# Babel 转码器
---
+++ JavaScript/Babel/index.es5.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var _test = require('./assets/test.js');
+
+var _test2 = _interopRequireDefault(_test);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+window.a3 = _test2.default;
